2c94e55b158269c06dcdd5ce8b40d765b594375b
Futureblur <itsfutureblur@gmail.com>
2022-04-13 20:49:56 +0200

    Update responsive design

--- v3/components/Button.js
+++ v3/components/Button.js
@@ -1,14 +1,31 @@
 import styles from "../styles/Button.module.css"
 
-export default function Button({ text, link, isFunc, top, bottom, width }) {
+export default function Button({
+								   text,
+								   link,
+								   isFunc,
+								   top,
+								   bottom,
+								   width,
+								   color = "var(--color-fg)",
+								   textColor = "var(--color-bg)",
+								   borderColor,
+								   borderWidth = 2
+							   }) {
 	return (
 		<button className={ styles.btn }
 				onClick={ isFunc ? link : () => {
 					link ? window.location.href = link : ""
 				} }
-				style={ { marginTop: top, marginBottom: bottom, width: width } }>
-			<p
-				className={ styles.text }> { text }
+				style={ {
+					marginTop: top,
+					marginBottom: bottom,
+					width: width,
+					border: `${ borderWidth }px solid ${ borderColor }`,
+					background: color
+				} }>
+			<p className={ styles.text } style={ { background: color, color: textColor } }>
+				{ text }
 			</p>
 		</button>
 	)
--- v3/components/Header.js
+++ v3/components/Header.js
@@ -2,42 +2,45 @@ import styles from "../styles/Header.module.css"
 import { useState } from "react";
 
 export default function Header() {
-    const [navBarActive, setActive] = useState(false);
-    const toggleNavBar = () => {
-        setActive(!navBarActive)
-    }
+	const [navBarActive, setActive] = useState(false);
+	const toggleNavBar = () => {
+		setActive(!navBarActive)
+	}
 
-    return <>
-        <div className={ styles.navBarWrapper }>
-            <div className={ styles.navBar }>
-                <div className={ styles.left }>
-                    <a href={ "https://futureblur.com" } style={ { display: "block" } }>
-                        <img className={ styles.logo } src={ "../futureblur-light.png" } alt={ "futureblur logo" }/>
-                    </a>
-                </div>
-                {
-                    navBarActive ?
-                        <div className={ styles.burgerActive } onClick={ toggleNavBar }/> :
-                        <div className={ styles.burger } onClick={ toggleNavBar }/>
-                }
-                {
-                    navBarActive ?
-                        <div className={ styles.rightActive }>
-                            <a className={ styles.navLink } href={ "https://instagram.com/itsfutureblur" }>Virtual
-                                Gallery</a>
-                            <a className={ styles.navLink } href={ "#" }>About</a>
-                            <a className={ styles.navLink } href={ "https://futureblur.com/contact" }>Contact</a>
-                        </div> :
+	return (
+		<div className={ styles.navBarWrapper }>
+			<div className={ styles.navBar }>
+				<div className={ styles.left }>
+					<a href={ "https://futureblur.com" } style={ { display: "block" } }>
+						<img className={ styles.logo } src={ "../Futureblur_Logo_White.svg" }
+							 alt={ "Futureblur Logo" }/>
+						<img className={ styles.logoIcon } src={ "../Futureblur_Icon_White.svg" }
+							 alt={ "Futureblur Logo" }/>
+					</a>
+				</div>
+				{
+					navBarActive ?
+						<div className={ styles.burgerActive } onClick={ toggleNavBar }/> :
+						<div className={ styles.burger } onClick={ toggleNavBar }/>
+				}
+				{
+					navBarActive ?
+						<div className={ styles.rightActive }>
+							<a className={ styles.navLink } href={ "https://instagram.com/itsfutureblur" }>Virtual
+								Gallery</a>
+							<a className={ styles.navLink } href={ "#" }>About</a>
+							<a className={ styles.navLink } href={ "https://futureblur.com/contact" }>Contact</a>
+						</div> :
 
-                        <div className={ styles.right }>
-                            <a className={ styles.navLink } href={ "https://instagram.com/itsfutureblur" }>Virtual
-                                Gallery</a>
-                            <a className={ styles.navLink } href={ "#" }>About</a>
-                            <a className={ styles.navLink } href={ "https://futureblur.com/contact" }>Contact</a>
-                        </div>
-                }
+						<div className={ styles.right }>
+							<a className={ styles.navLink } href={ "https://instagram.com/itsfutureblur" }>Virtual
+								Gallery</a>
+							<a className={ styles.navLink } href={ "#" }>About</a>
+							<a className={ styles.navLink } href={ "https://futureblur.com/contact" }>Contact</a>
+						</div>
+				}
 
-            </div>
-        </div>
-    </>
+			</div>
+		</div>
+	)
 }
