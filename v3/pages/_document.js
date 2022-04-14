cbea52dd9eeb47a570fb91102cd601a80d553cd1
Futureblur <itsfutureblur@gmail.com>
2022-04-14 13:00:06 +0200

    Update favicon color

--- v3/pages/_document.js
+++ v3/pages/_document.js
@@ -10,7 +10,7 @@ class AppDocument extends Document {
 					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 					<link rel="manifest" href="/site.webmanifest"/>
-					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#871504"/>
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
 					<meta name="msapplication-TileColor" content="#6965ff"/>
 					<meta name="theme-color" content="#000000"/>
 
