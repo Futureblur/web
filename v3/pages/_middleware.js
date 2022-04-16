e8e664bf152bf2e2023511d949892760e6a1c515
Futureblur <itsfutureblur@gmail.com>
2022-04-16 12:53:09 +0200

    Add more links

--- v3/pages/_middleware.js
+++ v3/pages/_middleware.js
@@ -6,10 +6,17 @@ const urls = [
 	["youtube", "https://youtube.com/futureblur"],
 	["github", "https://github.com/futureblur"],
 	["twitch", "https://twitch.com/futureblur"],
+	["discord", "https://discord.gg/FK2mh7P72Y"],
+	["dsc", "https://discord.gg/FK2mh7P72Y"],
+	["sub", "https://youtube.com/c/Futureblur?sub_confirmation=1"],
+	["subscribe", "https://youtube.com/c/Futureblur?sub_confirmation=1"],
 	["artstation", "https://artstation.com/futureblur"],
 	["reddit", "https://reddit.com/u/futureblur"],
 	["linktree", "https://linktree.com/futureblur"],
 	["spotify", "https://open.spotify.com/artist/3ot0eoiEubcohJl91uERYs"],
+	["id", "https://youtu.be/8Q8gstqYST0"],
+	["retroglitch", "https://youtu.be/89iXrz1Cdt8"],
+	["badlands", "https://youtu.be/0_mZrW5zunE"],
 ]
 
 export default async function Middleware(req) {
