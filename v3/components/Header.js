0fc2d14de69cfe8f2d350ee413cdb8bdd4715ce1
Futureblur <itsfutureblur@gmail.com>
2022-04-16 13:09:14 +0200

    Move some parts into separate components

--- v3/components/Header.js
+++ v3/components/Header.js
@@ -13,44 +13,62 @@ export default function Header() {
 			<div className={ styles.navBar }>
 				<div className={ styles.left }>
 					<a href={ "https://futureblur.com" } style={ { display: "flex" } }>
-						{/*<img className={ styles.logo } src={ "../images/Futureblur_Logo_White.svg" }*/ }
-						{/*	 alt={ "Futureblur Logo" }/>*/ }
 						<div className={ styles.logo }>
-							<ExportedImage src={ "../images/Futureblur_Logo_White.svg" } alt={ "Futureblur Logo" }
-										   layout={ "fixed" } width={ 188 } height={ 30 }/>
+							<Logo/>
 						</div>
 						<div className={ styles.logoIcon }>
-							<ExportedImage src={ "../images/Futureblur_Icon_White.svg" } alt={ "Futureblur Logo" }
-										   layout={ "fixed" } width={ 64 } height={ 30 }/>
-
-							{/*<img className={ styles.logoIcon } src={ "../images/Futureblur_Icon_White.svg" }*/ }
-							{/*	 alt={ "Futureblur Logo" }/>*/ }
+							<LogoIcon/>
 						</div>
 					</a>
 				</div>
-				{
-					navBarActive ?
-						<div className={ styles.burgerActive } onClick={ toggleNavBar }/> :
-						<div className={ styles.burger } onClick={ toggleNavBar }/>
-				}
-				{
-					navBarActive ?
-						<div className={ styles.rightActive }>
-							<a className={ styles.navLink } href={ "https://instagram.com/itsfutureblur" }>Virtual
-								Gallery</a>
-							<a className={ styles.navLink } href={ "#" }>About</a>
-							<a className={ styles.navLink } href={ "https://futureblur.com/contact" }>Contact</a>
-						</div> :
-
-						<div className={ styles.right }>
-							<a className={ styles.navLink } href={ "https://instagram.com/itsfutureblur" }>Virtual
-								Gallery</a>
-							<a className={ styles.navLink } href={ "#" }>About</a>
-							<a className={ styles.navLink } href={ "https://futureblur.com/contact" }>Contact</a>
-						</div>
-				}
-
+				<NavBarRight navBarActive={ navBarActive } toggleNavBar={ toggleNavBar }/>
 			</div>
 		</div>
+	)
+}
+
+function NavBarRight({ navBarActive, toggleNavBar }) {
+	return (
+		<>
+			{
+				navBarActive ?
+					<div className={ styles.burgerActive } onClick={ toggleNavBar }/> :
+					<div className={ styles.burger } onClick={ toggleNavBar }/>
+			}
+			{
+				navBarActive ?
+					<div className={ styles.rightActive }>
+						<NavBarLinks/>
+					</div> :
+					<div className={ styles.right }>
+						<NavBarLinks/>
+					</div>
+			}
+		</>
+	)
+}
+
+function NavBarLinks() {
+	return (
+		<>
+			<a className={ styles.navLink } href={ "https://instagram.com/itsfutureblur" }>Virtual
+				Gallery</a>
+			<a className={ styles.navLink } href={ "https://futureblur.com/github" }>Open Source</a>
+			<a className={ styles.navLink } href={ "https://futureblur.com/contact" }>Contact</a>
+		</>
+	)
+}
+
+function Logo() {
+	return (
+		<ExportedImage src={ "../images/Futureblur_Logo_White.svg" } alt={ "Futureblur Logo" }
+					   layout={ "fixed" } width={ 188 } height={ 30 }/>
+	)
+}
+
+function LogoIcon() {
+	return (
+		<ExportedImage src={ "../images/Futureblur_Icon_White.svg" } alt={ "Futureblur Logo" }
+					   layout={ "fixed" } width={ 64 } height={ 30 }/>
 	)
 }
