252d805b023736002cf72813c2c9da456ce1cbba
Futureblur <rasul@futureblur.com>
2023-12-27 09:41:55 +0100

    ci(astro): update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://futureblur.github.io',
+    site: 'https://futureblur.github.io/web',
     integrations: [tailwind()]
 });
