15dd4efcdddae117b7cb0d11e592cebf27bc57cf
Futureblur <rasul@futureblur.com>
2023-12-27 08:49:14 +0100

    ci(astro): remove subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://beta.futureblur.com',
+    site: 'https://futureblur.com',
     integrations: [tailwind()]
 });
