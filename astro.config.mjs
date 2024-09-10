f3a30f8f28a0a4db7050f99820a9fff28e3ae009
rds <rasul@futureblur.com>
2024-09-10 17:13:00 +0200

    blank slate

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://futureblur.github.io/web',
-    integrations: [tailwind()]
-});
