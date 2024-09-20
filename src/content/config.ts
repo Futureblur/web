08ad030efca7b4ed93b53030ea5d3ae34cb7f650
rds <rasul@futureblur.com>
2024-09-20 21:34:47 +0200

    fix: cover image not loading for meta tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,23 @@
 import { defineCollection, reference, z } from 'astro:content';
 
-const baseSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    cover: z.string(),
-    alt: z.string()
+const newsCollection = defineCollection({
+    type: 'content',
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        description: z.string(),
+        cover: image().refine((img) => img.width >= 1080, {
+            message: 'Cover image must be at least 1080px wide',
+        }),
+        alt: z.string(),
+        date: z.coerce.date({ message: 'Invalid date' }),
+        tagline: z.string(),
+        caption: z.string(),
+        author: reference('authors'),
+        draft: z.boolean({ message: 'Draft mode was not specified' }),
+        type: z.enum(['Tip', 'Story', 'Announcement']),
+        url: z.string().url({ message: 'You need to specify a valid url' }),
+    })
 })
-
-const newsCollection = baseSchema.extend({
-    date: z.coerce.date({ message: 'Invalid date' }),
-    tagline: z.string(),
-    caption: z.string(),
-    author: reference('authors'),
-    draft: z.boolean({ message: 'Draft mode was not specified' }),
-    type: z.enum(['Tip', 'Story', 'Announcement']),
-    url: z.string().url({ message: 'You need to specify a valid url' }),
-});
 
 const authorsCollection = defineCollection({
     type: 'data',
@@ -38,7 +40,7 @@ const markdownCollection = defineCollection({
 })
 
 export const collections = {
-    news: defineCollection({ type: 'content', schema: newsCollection }),
+    news: newsCollection,
     authors: authorsCollection,
     markdown: markdownCollection,
 };
