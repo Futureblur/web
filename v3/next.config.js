36d4396db84c02ed84d7da3e65f1ad30589b1ec8
rds <rasul@futureblur.com>
2024-09-24 12:13:32 +0200

    refactor: remove ci and update build

--- v3/next.config.js
+++ v3/next.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		loader: "custom",
-		nextImageExportOptimizer: {
-			imageFolderPath: "public/images",
-			exportFolderPath: "out",
-			imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-			deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-			quality: 75,
-		},
-	},
-	env: {
-		storePicturesInWEBP: true,
-	},
+    reactStrictMode: true,
+    assetPrefix: '/v3',
+    images: {
+        loader: 'custom',
+        nextImageExportOptimizer: {
+            imageFolderPath: 'public/images',
+            exportFolderPath: 'out',
+            imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+            deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+            quality: 75,
+        },
+    },
+    env: {
+        storePicturesInWEBP: true,
+    },
 }
