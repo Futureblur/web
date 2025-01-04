990444b51d76587bf0454ea1a45eff90ac38b0a7
rds <rasul@futureblur.com>
2025-01-04 14:53:27 +0100

    feat: add investors tag

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,7 +14,7 @@ const newsCollection = defineCollection({
         caption: z.string(),
         author: reference('authors'),
         draft: z.boolean({ message: 'Draft mode was not specified' }),
-        type: z.enum(['Announcement', 'Tip', 'Update', 'Story']),
+        type: z.enum(['Announcement', 'Tip', 'Update', 'Story', 'Investors']),
         url: z.string().url({ message: 'You need to specify a valid url' }),
     })
 })
