179d2fb1b5ea68c9bec2716c46ae2517a8517240
rds <rasul@futureblur.com>
2025-04-14 18:18:03 +0200

    refactor(i18n): move getTranslatedSlug into i18n

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,3 @@
-import type { Language } from '@i18n/ui';
-import { getLangFromUrl, useTranslations } from '@i18n/utils';
-import type { CollectionEntry } from 'astro:content';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -8,45 +5,3 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export function formatDate(date: Date) {
-    return Intl
-        .DateTimeFormat('en-US', {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-        })
-        .format(date);
-}
-
-export function readingTime(html: string, lang: Language) {
-    const textOnly = html.replace(/<[^>]+>/g, '');
-    const wordCount = textOnly.split(/\s+/).length;
-    const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
-
-    const t = useTranslations(lang);
-    return `${readingTimeMinutes} ${t('post.readTime')}`;
-}
-
-export function getUsername(url: string) {
-    let n = url.lastIndexOf('/');
-    return url.substring(n + 1);
-}
-
-export function getTranslatedSlug(slug: string, basePath: string = '') {
-    // Remove first slash if exists
-    if (slug.charAt(0) === '/') {
-        slug = slug.slice(1);
-    }
-
-    let lang = getLangFromUrl(new URL(`https://futureblur.com/${slug}`));
-    let removedSlug = slug.replace(`${lang}`, '');
-    return `/${lang}${basePath}${removedSlug}`;
-}
-
-// Returns false for the first 3 elements of news.
-export function shouldLoadCardLazy(
-    news: Array<CollectionEntry<'news'>>,
-    post: CollectionEntry<'news'>,
-) {
-    return news.includes(post, 3);
-}
